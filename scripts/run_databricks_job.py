9ee067ee9bdbfc05aaf4c66ecd8da11667e41fa5
Colton Padden <colton@dagsterlabs.com>
2025-08-07 14:16:41 -0400

    wip

--- scripts/run_databricks_job.py
+++ scripts/run_databricks_job.py
@@ -4,16 +4,21 @@ import dagster as dg
 from databricks.sdk import WorkspaceClient
 from dataclasses import dataclass
 
+# Access Databricks environment connection details from environment variables
 DATABRICKS_HOST = os.environ.get("DATABRICKS_HOST")
 DATABRICKS_TOKEN = os.environ.get("DATABRICKS_TOKEN")
 
 if __name__ == "__main__":
+    # The ID of the job we are running in Databricks that produces CSV reporting files
     databricks_job_id = 1000180891217799
+
+    # The parameters being passed to our notebook job
     job_parameters = {
         source_file_prefix: "s3://acme-analytics/raw"
         destination_file_prefix: "s3://acme-analytics/reports"
     }
 
+    # Run our job and wait for completion using the `databricks-sdk`
     client = WorkspaceClient(host=self.host, token=self.token)
     client.jobs.run_now_and_wait(
         job_id=databricks_job_id,
